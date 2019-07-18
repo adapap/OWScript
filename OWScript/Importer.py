a65e225464dc94daf165f1f0066f70a5a0ca610a
Matthew Hatcher <matthewh@halfpetal.com>
2019-07-18 03:55:44 -0400

    Add basic importing. Non-recursive for now.

---
+++ OWScript/Importer.py
@@ -0,0 +1,20 @@
+import os
+
+class Importer:
+    def __init__(self, file, text):
+        self.file = file
+        self.text = text
+
+    def run(self):
+        path = os.path.dirname(self.file.name)
+        lines = self.text.split('\n')
+
+        for i, line in enumerate(lines):
+            if '#import ' in line:
+                importSplit = line.split(' ')
+                importFile = importSplit[1].replace('\'', '') + '.owpy'
+                f = open(os.path.join(path, importFile))
+                importText = f.read()
+                lines[i] = importText
+
+        return '\n'.join(lines)
